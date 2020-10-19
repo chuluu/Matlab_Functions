891d7259721a679587e72619e34d2219b4671113
chuluu <64043757+chuluu@users.noreply.github.com>
2020-10-19 18:46:08 -0400

    Add files via upload

---
+++ Functions/MyAvgGxx_Overlap.m
@@ -0,0 +1,55 @@
+function [Gxx_avg,f_Gxx] = MyAvgGxx_Overlap(xn,NSTFT,overlap,fs)
+% Inputs:
+% xn         = Input Vector Time Array
+% NSTFT      = Length of each record desired
+% overlap    = Overlap percentage
+% fs         = Sampling Rate
+% Outputs:
+% Gxx_Avg    = Average Gxx from the operation
+% f_Gxx      = Frequency of the Gxx average
+
+N_overlap  = round(overlap*NSTFT);        % Overlap sample length
+N          = length(xn); % length of total array
+
+% Condition for when overlap is zero (i.e. no overlap)
+if N_overlap == 0
+    NSTFT_real = NSTFT;
+else
+    NSTFT_real = NSTFT - N_overlap;
+end
+
+if overlap == 0 
+    NS       = round(N/NSTFT_real);
+elseif overlap < 0.51
+    NS       = round(N/NSTFT_real)-2; % General Number of STFT times 
+elseif overlap < 0.76
+    NS       = round(N/NSTFT_real)-4;
+elseif overlap < 0.86
+    NS       = round(N/NSTFT_real)-7;
+elseif overlap < 0.91
+    NS       = round(N/NSTFT_real)-10;
+end
+
+xn_array = zeros(NS-1,NSTFT);     % Per-allocate
+L_window = NSTFT;               % Window lengths
+window   = hann(L_window,'periodic')';     % Window
+ms = sum(window.*window)/L_window;
+window = window/sqrt(ms);
+
+% Begin B&B
+xn_array(1,:) = xn(1:NSTFT);
+[Gxx,Sxx,f_Sxx,f_Gxx] = MyPSDX(xn_array(1,:),fs);
+Gxx_array(1,:) = Gxx;
+for a = 2:NS
+    SC  = (NSTFT-N_overlap)*(a-1);
+    xn_array(a,:) = xn(1+SC:NSTFT+SC).*window;
+    [Gxx,Sxx,f_Sxx,f_Gxx] = MyPSDX(xn_array(a,:),fs);
+    Gxx_array(a,:) = Gxx;
+end
+
+Gxx_avg = sum(Gxx_array,1)./NS;
+df = fs/NSTFT;
+RMS = sqrt(sum(Gxx_avg).*df);
+disp(['The RMS: ', num2str(RMS)]);
+end
+
