a0ce2db9a5e1cd517e64dd9962bc7f523a065bba
chuluu <64043757+chuluu@users.noreply.github.com>
2020-10-28 17:49:15 -0400

    Add files via upload

---
+++ Function_Packages/+MyGen/Analog_TF_Analyzer.m
@@ -0,0 +1,29 @@
+function [H,P,Z,Hw] = Analog_TF_Analyzer(num,den,f,fig_num)
+% [H,P,Z,Hw] = Analog_TF_Analyzer(num,den,f)
+    w   = 2*pi*f;
+    H = tf(num,den);
+    P = pole(H);
+    Z = zero(H);
+    poles_old = 0;
+    zeros_old = 0;
+
+    for a = 0:length(num)-1
+        sub = num(a+1).*((1i.*w).^a);
+        zeros_fnc = zeros_old + sub;
+        zeros_old = zeros_fnc;
+    end
+
+    for a = 0:length(den)-1
+        sub = den(a+1).*((1i.*w).^a);
+        poles_fnc = poles_old + sub;
+        poles_old = poles_fnc;
+    end
+    Hw  = zeros_fnc./poles_fnc;
+    if nargin < 4
+        
+    else
+        figure(fig_num);
+        bode(H);
+    end
+
+end
